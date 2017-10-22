34ed5bac94c21378f5f6f3285c5871431b7a9e6e
Валерійка <kudravka13@gmail.com>
2017-10-22 20:30:26 +0300

    Home work 2 fix

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-var array = [2, 5, 1, 3, 1, 2, 1, 7, 7, 6];
+var array = [2, 2, 1, 2, 2, 3, 0, 1, 2] // 4
+//[7, 0, 1, 3, 4, 1, 2, 1] // 9
+  //  [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0] //10
+// [2, 2, 2, 2, 2] // 0
+
+
 var max_one = 0,
     max_two = array[0],
     index_one = 0,
@@ -7,45 +12,52 @@ var max_one = 0,
     water_two,
     water = 0;
 
-for (i = index_one; i < array.length; i++) {
-    if (max_one < array[i]) {
-        max_one = array[i];
-        index_one = i;
-        break;
-    }
-}
-//
-do {
+function findWather(arr) {
     for (i = index_one; i < array.length; i++) {
-
-        if ((array[i - 1] < array[i]) && (array[i] !== max_one) && ((array[i] > array[i + 1]) || (array[i] === array[i + 1]))) {
-            max_two = array[i];
-            index_two = i;
+        if (max_one < array[i]) {
+            max_one = array[i];
+            index_one = i;
             break;
-        } else if ((array[i - 1] < array[i]) && (array[i] !== max_one) && (i === array.length - 1)) {
-            max_two = array[i];
-            index_two = i;
-            break;
-        } else {
-            continue;
         }
     }
-    if (max_one === max_two) {
-        index_two = array.length;
-        continue;
-    }
+    do {
+        for (var i = index_one; i < array.length; i++) {
 
-    water_two = max_one < max_two ? max_one : max_two;
-    count = 0;
-    for (var i = index_one + 1; i < index_two; i++) {
-        count = count + array[i];
-        console.log('count = ', count)
-    }
+            if ((array[i - 1] < array[i]) &&
+                (array[i] !== max_one) &&
+                ((array[i] > array[i + 1]) || (array[i] === array[i + 1]))) {
+                    max_two = array[i];
+                    index_two = i;
+                    break;
 
-    water += ((index_two - 1 - index_one) * water_two) - count;
+            } else {
+                if ((array[i - 1] < array[i]) && (array[i] !== max_one) && (i === array.length - 1)) {
+                    max_two = array[i];
+                    index_two = i;
+                    break;
+                }
+
+                console.log(max_two)
+            }
+        }
+        if (max_one === max_two) {
+            index_two = array.length;
+            continue;
+        }
+
+        water_two = max_one < max_two ? max_one : max_two;
+        count = 0;
+        for (var i = index_one + 1; i < index_two; i++) {
+            count = count + array[i];
+        }
+
+        water += ((index_two - 1 - index_one) * water_two) - count;
+        max_one = max_two;
+        index_one = index_two;
+    }
+    while (index_two < array.length) ;
     console.log('water = ', water);
-    max_one = max_two;
-    index_one = index_two;
-} while (index_two < array.length);
 
+}
 
+findWather(array);
