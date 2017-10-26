ea64369b542f14f29a5fa84ae443a31e35760145
LeraB <kudravka13@gmail.com>
2017-10-26 11:05:14 +0300

    added an aisle on the other side of the array

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-var array = [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 8]
+var array = [0,0,0,0,1000,0,1001,1,0,1]
 //[7, 0, 1, 3, 4, 1, 2, 1] // 9
 //  [2, 1, 5, 0, 3, 4, 7, 2, 3, 1, 0] //10
 // [2, 2, 2, 2, 2] // 0
@@ -7,39 +7,50 @@ function findWather(array) {
 
     var max_one =  array[0],
         index_one = 0,
-        max_two = array[0],
+        max_two = array[array.length-1],
         index_two = 0,
         count = 0,
-        water_one = 0
+        water_one = 0,
         water_two = 0,
         water = 0;
 
+
     do{
+
     for (var i = 0; i < array.length; i++) {
-        if (array[i] >= max_one) {
-            max_one = array[i];
-            water = water_one
-            console.log(max_one)
-        } else {
-            water_one += max_one - array[i]
+            if (array[i] >= max_one) {
+                water += max_one * index_one - water_one;
+                water_one = 0;
+                index_one = 0;
+                max_one = array[i];
+                console.log(max_one);
+            } else {
+                water_one += array[i];
+                index_one += 1;
+            }
         }
-    }
+
 
     for ( i = array.length-1; i >= 0; i--) {
-        if (array[i] > max_two) {
-            max_two = array[i];
-            water += water_two;
-            console.log(max_two)
-        } else {
-            water_two += max_two - array[i]
+            if (array[i] > max_two) {
+                water += max_two * index_two - water_two;
+                water_two = 0;
+                index_two = 0;
+                max_two = array[i];
+                console.log(max_two)
+            } else {
+                water_two += array[i];
+                index_two +=1;
+            }
         }
+
         if (max_one === max_two) {
             index_two = array.length;
             continue;
         }
-    }
+
 } while (index_two < array.length);
-    console.log(water)
+    console.log('water = ', water);
 }
 
 findWather(array);
